f46fed9a97bd867e657fef004e40cfd61af7a3c8
Ville Svärd <ville.svard@agical.se>
2018-09-23 19:49:47 +0200

    Add port output etc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,31 @@
 import greeter.GreeterServer;
 
 import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Arrays;
+
+import static java.text.MessageFormat.format;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            GreeterServer.start(port(args))
+            int port = port(args);
+            System.out.println(format("Starting server at port {0,number,#}", port));
+            GreeterServer.start(port)
                     .waitIndefinitely();
         } catch (IOException e) {
-            System.out.println("Failed to start server because: " + e.getMessage());
+            System.out.println(format("Failed to start server because: {0}", e.getMessage()));
         } catch (NumberFormatException e) {
-            System.out.println("Failed to parse desired port: " + e.getMessage());
+            System.out.println(format("Failed to parse desired port: {0}", e.getMessage()));
         }
     }
 
     private static int port(String[] args) throws NumberFormatException {
-        if (args.length > 0) {
-            return Integer.parseInt(args[0]);
-        } else {
-            return 1234;
-        }
+        return Arrays.stream(args)
+                .limit(1)
+                .map(Integer::parseInt)
+                .findFirst()
+                .orElse(1234);
     }
 
 }
